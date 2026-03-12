d541004935a9c7a5b5dea501fe05aa47473dc810
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-03-12 23:51:24 +0100

    added pin arm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
 
+const int PIN_BASE      = A0;
+const int PIN_SHOULDER  = A1;
+const int PIN_ELBOW     = A2;
+const int PIN_WRIST     = A3;
+const int PIN_GRIP      = A6;
+
 void setup(){
     // setup
 
