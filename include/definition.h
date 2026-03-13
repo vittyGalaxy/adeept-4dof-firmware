33307f712aa25201324cb38816f4d11c4c648616
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-03-14 00:05:37 +0100

    feat: create file definition

---
+++ include/definition.h
@@ -0,0 +1,18 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+// potentiometers
+const int PIN_POTENTIOMETERS_BASE      = A0;
+const int PIN_POTENTIOMETERS_SHOULDER  = A1;
+const int PIN_POTENTIOMETERS_ELBOW     = A2;
+const int PIN_POTENTIOMETERS_WRIST     = A3;
+const int PIN_POTENTIOMETERS_GRIP      = A6;
+
+const int PIN_BASE = 9;
+const int PIN_SHOULDER = 8;
+const int PIN_ELBOW = 7;
+const int PIN_WRIST = 6;
+const int PIN_GRIP = 5;
+
+
+#endif //DEFINITIONS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include <Arduino.h>
-
-const int PIN_BASE      = A0;
-const int PIN_SHOULDER  = A1;
-const int PIN_ELBOW     = A2;
-const int PIN_WRIST     = A3;
-const int PIN_GRIP      = A6;
+#include <Servo.h>
 
 void setup(){
     // setup
