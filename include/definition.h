8176415a1aa3adaf55cbce7753701148916e2846
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-03-19 23:49:54 +0100

    fix the pins

--- include/definition.h
+++ include/definition.h
@@ -1,18 +1,19 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-// potentiometers
+// Potentiometers
 const int PIN_POTENTIOMETERS_BASE      = 14;
 const int PIN_POTENTIOMETERS_SHOULDER  = 15;
 const int PIN_POTENTIOMETERS_ELBOW     = 16;
 const int PIN_POTENTIOMETERS_WRIST     = 17;
 const int PIN_POTENTIOMETERS_GRIP      = 20;
 
+// Servo
 const int PIN_BASE                     = 9;
-const int PIN_SHOULDER                 = 11;
-const int PIN_ELBOW                    = 3;
-const int PIN_WRIST                    = 6;
-const int PIN_GRIP                     = 5;
+const int PIN_SHOULDER                 = 6;
+const int PIN_ELBOW                    = 5;
+const int PIN_WRIST                    = 3;
+const int PIN_GRIP                     = 11;
 
 
 #endif //DEFINITIONS_H
